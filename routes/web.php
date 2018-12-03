3a20c2b92f30b493997254ecaf1be9d146bd9d4b
ulrich217 <ulrichcedric@yahoo.fr>
2018-12-03 19:39:54 +0000

    debug of

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('1', function () {return 'je suis la page 1 !';});
+Route::get('2', function () {return 'je suis la page 2 !';});
+Route::get('3', function () {return 'je suis la page 3 !';});
